89a353925bd654dcbf2829c246456eb5caff30ae
pranjulknit <pranjul.19641@knit.ac.in>
2022-01-02 19:56:26 +0530

    updated UI

--- lib/authenticate/login_screen.dart
+++ lib/authenticate/login_screen.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors, dead_code, unnecessary_new
 
 import 'package:flutter/material.dart';
 
@@ -14,192 +14,164 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Container(
-          height: size.height,
-          width: size.width,
-          decoration: BoxDecoration(
-            gradient: LinearGradient(colors: [
-              Color.fromRGBO(226, 56, 70, 1),
-              Color.fromRGBO(217, 39, 79, 1),
-            ]),
-          ),
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                Container(
-                  height: size.height / 3,
-                  width: size.width,
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                        image: AssetImage("assets/images/log.png"),
-                        fit: BoxFit.cover),
-                  ),
-                ),
-                SizedBox(
-                  height: size.height / 40,
-                ),
-                field(size),
-                SizedBox(
-                  height: size.height / 40,
-                ),
-                Container(
-                  height: size.height / 14,
-                  width: size.width / 1.12,
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                    color: Colors.black,
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Text(
-                    "Send OTP",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18,
-                    ),
-                  ),
-                ),
-                Container(
-                  height: size.height / 8,
-                  width: size.width,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Container(
-                        height: 2,
-                        width: size.width / 3,
-                        color: Color.fromRGBO(239, 79, 95, 1),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(
-                          "OR",
-                          style: TextStyle(
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
-                      Container(
-                        height: 2,
-                        width: size.width / 3,
-                        color: Color.fromRGBO(239, 79, 95, 1),
-                      ),
-                    ],
-                  ),
-                ),
-                loginWithEmailButton(size),
-                SizedBox(
-                  height: size.height / 40,
-                ),
-
-                Container(
-                  height: size.height / 15,
-                  width: size.width / 1.12,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      customButton(
-                          size, "Facebook", "assets/images/facebook.png"),
-                      customButton(size, "Google", "assets/images/google.png"),
-                    ],
-                  ),
-                ),
-                SizedBox(height: size.height / 25),
-                Text(
-                  "By Continuing, you agree to our",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 12,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(4.0),
-                  child: Text(
-                    "Term of Services Privacy Policy Content Policy",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 12,
-                    ),
-                  ),
-                )
-                //customButton(size, "Facebook", "assets/images/facebook.png"),
-              ],
-            ),
-          )),
-    );
-  }
-
-  Widget customButton(Size size, String title, String path) {
-    return Container(
-      height: size.height / 14,
-      width: size.width / 2.3,
+        body: Container(
+      height: size.height,
+      width: size.width,
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
         color: Colors.white,
       ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-            height: size.height / 25,
-            width: size.height / 25,
-            decoration: BoxDecoration(
-              image:
-                  DecorationImage(image: AssetImage(path), fit: BoxFit.cover),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: size.height / 2.2,
+              width: size.width,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage("assets/images/log.jpg"),
+                    fit: BoxFit.cover),
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(title,
+            SizedBox(height: 20),
+            Text(
+              "India's #1 Food Delivery",
+              style: TextStyle(
+                fontWeight: FontWeight.w900,
+                fontSize: 26,
+              ),
+            ),
+            Text(
+              "and Dining App",
+              style: TextStyle(
+                fontWeight: FontWeight.w900,
+                fontSize: 26,
+              ),
+            ),
+            Container(
+              height: size.height / 12,
+              width: size.width,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    height: 2,
+                    width: size.width / 3,
+                    color: Colors.black12,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      "Log in or sign up",
+                      style: TextStyle(
+                        color: Colors.black87,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 2,
+                    width: size.width / 3,
+                    color: Colors.black12,
+                  ),
+                ],
+              ),
+            ),
+            field(size),
+            SizedBox(
+              height: size.height / 40,
+            ),
+            ContinueButton(size),
+            Container(
+              height: size.height / 12,
+              width: size.width,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    height: 2,
+                    width: size.width / 3,
+                    color: Colors.black12,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      "or",
+                      style: TextStyle(
+                        color: Colors.black87,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 2,
+                    width: size.width / 3,
+                    color: Colors.black12,
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  circleImage(size, "assets/images/google.png"),
+                  circleImage(size, "assets/images/3dot.jpg"),
+                ],
+              ),
+            ),
+            SizedBox(height: size.height / 25),
+            Text(
+              "By Continuing, you agree to our",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 12,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: Text(
+                "Terms of Services Privacy Policy Content Policy",
                 style: TextStyle(
-                  fontSize: 18,
-                )),
-          ),
-        ],
+                  color: Colors.black,
+                  fontSize: 12,
+                ),
+              ),
+            )
+          ],
+        ),
       ),
-    );
+    ));
   }
 
-  Widget loginWithEmailButton(Size size) {
+  Widget ContinueButton(Size size) {
     return Container(
-        height: size.height / 14,
+        height: size.height / 16,
         width: size.width / 1.12,
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(10),
-          color: Colors.white,
+          color: Colors.redAccent,
         ),
         child: Row(
           children: [
             SizedBox(
-              width: size.width / 14,
-            ),
-            Container(
-              height: size.height / 25,
-              width: size.width / 15,
-              decoration: BoxDecoration(
-                  image: DecorationImage(
-                image: AssetImage("assets/images/email.png"),
-                fit: BoxFit.cover,
-              )),
+              width: size.width / 6,
             ),
             SizedBox(
               width: size.width / 6,
             ),
             Text(
-              "Continue with Email",
+              "Continue",
               style: TextStyle(
-                color: Colors.black,
-                fontSize: 18,
-                fontWeight: FontWeight.w500,
+                color: Colors.white,
+                fontSize: 20,
+                fontWeight: FontWeight.w900,
               ),
-            )
+            ),
           ],
         ));
   }
 
   Widget field(Size size) {
     return Container(
-      height: size.height / 14,
+      height: size.height / 22,
       width: size.width / 1.12,
       decoration: BoxDecoration(
         color: Colors.white,
@@ -245,6 +217,18 @@ class _LoginScreenState extends State<LoginScreen> {
             ),
           )
         ],
+      ),
+    );
+  }
+
+  Widget circleImage(Size size, String path) {
+    return Container(
+      width: size.height / 10,
+      height: size.width / 10,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        image: DecorationImage(fit: BoxFit.contain, image: AssetImage(path)),
+        color: Colors.black,
       ),
     );
   }
